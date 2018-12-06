8660fbfde2dbfc5dc80288c6e349d358a822b640
Nicole Martinez <nike.a.martinez@gmail.com>
2018-12-05 21:19:01 -0600

    put in software fix for signal interference from the pump

--- hardware.py
+++ hardware.py
@@ -25,23 +25,29 @@ class EncoderInput:
         #GPIO.add_event_callback(self.clk, lambda channel: print("RECEIVED NEXT WIDGET COMMAND"))
 
         #event for clicking button
-        GPIO.add_event_detect(self.btn, GPIO.FALLING, callback=self.handleEncoderPress, bouncetime=1000)
+        GPIO.add_event_detect(self.btn, GPIO.RISING, callback=self.handleEncoderPress, bouncetime=1000)
         # GPIO.add_event_callback(self.btn, lambda channel: print("HEY STEVE"))
 
+    def sampleChannel(self, channel, sampleTime):
+        now = time()
+        list = []     #Stores IO samples from encoder dt. (or whatever)
+
+        while time() <= now + sampleTime: #Get samples over 2mS period of IO 'dt' state; as many as possible
+            list = list + [GPIO.input(channel)]
+
+        return int(round(sum(list) / len(list)))
+
     def handleEncoderPress(self, channel):
-        self.btnCallback()
-        print("------encoder button callback begin---------")
+        state = self.sampleChannel(self.btn, 0.010) #sample 10 ms
+
+        if state:
+            print("------encoder button callback begin---------")
+            self.btnCallback()
+        else:
+            print("----------BUTTON PRESS IGNORED!-------------")
 
     def handleEncoder(self, channel): # channel is not used, but is required to handle the event.
-        now = time()  #Represents the time this encoder event occoured.
-        list = []     #Stores IO samples from encoder dt.
-        state = False #state of encoder 'dt' based on average from 'list'
-
-        while time() <= now + 0.002: #Get samples over 2mS period of IO 'dt' state; as many as possible
-            list = list + [GPIO.input(self.dt)]
-
-        # print(list)
-        state = int(round(sum(list) / len(list))) #Average the values in list (dt IO state).  Output is 0 or 1. Represents state of 'dt'
+        state = self.sampleChannel(self.dt, 0.002)
 
         if state: #If dt state is True, encoder is turning Counter Clockwise, pass -1 to callback
             self.turnCallback(-1)
